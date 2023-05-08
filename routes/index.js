7b88d3227423ee8773fa5d0b51b8068767593503
Connor S <constroh@gmail.com>
2023-05-08 13:25:16 -0500

    employeeRoutes.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use(require("./departmentRoutes"));
-// router.use(require('./partyRoutes'));
-// router.use(require('./voterRoutes'));
-// router.use(require('./voteRoutes'));
+router.use(require("./employeeRoutes"));
 
 module.exports = router;

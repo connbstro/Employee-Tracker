cefb377ca4ce130771b2eab1eccbc48f322ef71d
Connor S <constroh@gmail.com>
2023-05-08 12:12:17 -0500

    API routes

--- db/connection.js
+++ db/connection.js
@@ -4,11 +4,12 @@ const mysql = require("mysql2");
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: "127.0.0.1",
+    // host: "127.0.0.1",
+    host: "localhost",
     // mysql username
     user: "root",
     // mysql password
-    password: "",
+    password: "password123",
     database: "employee_tracker",
   },
   console.log(`Connected to the employee_tracker database.`)
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const inquirer = require("inquirer");
 const db = require("./db/connection");
+const apiRoutes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();

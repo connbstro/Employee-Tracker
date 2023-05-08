1ef93d84b35f0215077b791d712d214e5aaabd9f
Connor S <constroh@gmail.com>
2023-05-08 12:08:59 -0500

    update server.js

--- server.js
+++ server.js
@@ -9,16 +9,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Query the database
-db.query("SELECT * FROM department", function (err, results) {
-  console.log(results);
-});
+// // Query the database
+// db.query("SELECT * FROM department", function (err, results) {
+//   console.log(results);
+// });
+
+// Use API routes
+app.use("/api", apiRoutes);
 
 // Response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+  });
 });
